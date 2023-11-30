348efc63268d110ce1b0c7e1d2382bf514995424
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-11-30 11:02:02 +0530

    create component header & create and run server in folder api

--- 01 major projects/mern_authentication/client/src/App.jsx
+++ 01 major projects/mern_authentication/client/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages here
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
+// components here
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
