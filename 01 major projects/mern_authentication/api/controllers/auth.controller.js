ea5eb906fd7ad87c309c82aacf9bf50294a74df1
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-11-30 14:53:48 +0530

    add a middleware error handler and custom error handler
    add a middleware in index-js to handle errors and create utils folder and implement custom error handler in it.

--- 01 major projects/mern_authentication/api/controllers/auth.controller.js
+++ 01 major projects/mern_authentication/api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const userModel = require("../models/user.model.js");
 const bcryptjs = require("bcryptjs");
+const { errorHandler } = require("../utils/error.js");
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new userModel({ username, email, password: hashedPassword });
@@ -9,7 +10,11 @@ const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "new user created successfully" });
   } catch (err) {
-    res.status(500).json(err.message);
+    // res.status(500).json(err.message);
+
+    // instead upper line of code,
+    // use a middle ware in index.js to handle errors,
+    next(err);
   }
 };
 
--- 01 major projects/mern_authentication/api/index.js
+++ 01 major projects/mern_authentication/api/index.js
@@ -15,5 +15,10 @@ mongoose
 app.use(express.json()); // server can now accept json request
 app.use("/api/user", require("./routes/user.route.js"));
 app.use("/api/auth", require("./routes/auth.route.js"));
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.json({ success: false, message, statusCode });
+});
 
 app.listen(PORT, () => console.log(`Server Running on ${PORT}....`));
