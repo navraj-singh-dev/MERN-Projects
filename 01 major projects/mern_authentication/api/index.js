d6a59d5ac4867d8fe9fd86daac18819de7a0e8cb
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-11-30 11:31:06 +0530

    connect to db

--- 01 major projects/mern_authentication/api/index.js
+++ 01 major projects/mern_authentication/api/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 const PORT = 8080;
+
+// database connection
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Server Running on ${PORT}....`));
