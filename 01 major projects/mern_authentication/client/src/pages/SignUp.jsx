12ef65342c3c723df2c962816275335a6a048eeb
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-12-04 20:46:33 +0530

    complete the sign in page functionality
    connected react sign-in page to express backend, users can now sign-in using their credentials in react front end, useNavigate() is also implemented in Sign-Up and Sign-In react pages.

--- 01 major projects/mern_authentication/client/src/pages/SignUp.jsx
+++ 01 major projects/mern_authentication/client/src/pages/SignUp.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-
+  const navigate = useNavigate();
   const handleChange = (e) => {
     // event object is being passed as argument and target.id is coming from <input> tag's attribute.
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -30,6 +30,7 @@ export default function SignUp() {
         setError(true);
         return;
       }
+      navigate("/sign-in")
     } catch (error) {
       setLoading(false);
     }
