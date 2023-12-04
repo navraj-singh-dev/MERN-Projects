3196deb94804afe8aa95af1d95fcea540be36d6d
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-12-04 16:09:59 +0530

    create sign in api route
    coded entire backend for sign in, JWT token + cookie with expiration date added, validation about correct password (using bcryptjs) and correct email added, also tested the api route, working fine.

--- 01 major projects/mern_authentication/api/controllers/auth.controller.js
+++ 01 major projects/mern_authentication/api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model.js");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { errorHandler } = require("../utils/error.js");
 
 const signup = async (req, res, next) => {
@@ -18,4 +19,29 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await userModel.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User Email Not Found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // _doc means it is raw javascript object which is mongoDB document with no additional mongoose methods and data, bcuz mongoose attaches additonal information and methods to retrieved documents from mongoDB.
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    
+    // httpOnly:true option makes the cookie accessible only on the server side
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json({ rest });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { signup, signin };
