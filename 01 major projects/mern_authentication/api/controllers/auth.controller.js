0aeb9badd168a6c3a92cf79e368b3000b2584043
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-12-06 13:30:34 +0530

    create and add OAuth component
    new controller in backend : "google" (which do both sign-in and sign-up),
    new component : OAuth,
    firebase.js also added

--- 01 major projects/mern_authentication/api/controllers/auth.controller.js
+++ 01 major projects/mern_authentication/api/controllers/auth.controller.js
@@ -33,7 +33,7 @@ const signin = async (req, res, next) => {
     // _doc means it is raw javascript object which is mongoDB document with no additional mongoose methods and data, bcuz mongoose attaches additonal information and methods to retrieved documents from mongoDB.
     const { password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000); // 1 hour
-    
+
     // httpOnly:true option makes the cookie accessible only on the server side
     res
       .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
@@ -44,4 +44,48 @@ const signin = async (req, res, next) => {
   }
 };
 
-module.exports = { signup, signin };
+const google = async (req, res, next) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword, ...rest } = user._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new userModel({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-8),
+        email: req.body.email,
+        password: hashedPassword,
+        profilePicture: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword2, ...rest } = newUser._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin, google };
